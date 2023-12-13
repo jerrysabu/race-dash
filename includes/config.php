881d473bbdaf33fd1b921a5bd65095b57acf2a7e
Jerry Sabu <jerryaksa97@gmail.com>
2023-12-13 20:54:39 +0530

    database connewction done

---
+++ includes/config.php
@@ -0,0 +1,20 @@
+<?php
+
+// DB credentials.
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'race-studio-dash');
+//echo "connecdted ";
+// Establish database connection.
+try {
+    $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+    // $image_url = '../uploads/';
+    // $image_url_user = 'uploads/';
+    // $emailusername = 'jciedakkara@gmail.com';
+    // $emailpassword = 'goldenvalley';
+} catch (PDOException $e) {
+    exit("Error: " . $e->getMessage());
+}
+// echo "connected ";
+?>
